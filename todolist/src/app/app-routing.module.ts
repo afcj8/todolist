eeb7d86c2b9f9aba41b18b79e68ab575092f03be
afcj8 <contatoantoniojunior8@gmail.com>
2024-01-07 12:55:45 -0300

    Rotas

--- todolist/src/app/app-routing.module.ts
+++ todolist/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { DashboardComponent } from './componentes/dashboard/dashboard.component';
+import { ListaComponent } from './componentes/lista/lista.component';
+import { GradeComponent } from './componentes/grade/grade.component';
+
+const routes: Routes = [
+  {path:'', component: DashboardComponent},
+  {path:'lista', component: ListaComponent},
+  {path:'grade', component: GradeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
